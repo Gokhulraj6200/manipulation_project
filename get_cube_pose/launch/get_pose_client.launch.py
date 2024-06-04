f68dbe4fd311e5a4158917779d2b4d75dbc1161a
Gokhulraj6200 <gokhulraj6200@gmail.com>
2024-06-04 23:08:32 +0000

    get cube position using perception

---
+++ get_cube_pose/launch/get_pose_client.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    pkg_share_path = get_package_share_directory('get_cube_pose')
+    rviz_config_file = os.path.join(pkg_share_path, 'rviz', 'sim.rviz')
+
+    return LaunchDescription([
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_file]
+        ),
+        Node(
+            package='simple_grasping',
+            executable='basic_grasping_perception_node',
+            name='basic_grasping_perception_node',
+            output='screen',
+            parameters=[{'debug_topics': True}]
+        ),
+        Node(
+            package='get_cube_pose',
+            executable='get_pose_client',
+            name='get_pose_client',
+            output='screen'
+        ),
+    ])
